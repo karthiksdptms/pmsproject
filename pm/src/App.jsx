9b886fe8b6d368775171a8309b194024a52117ea
karthiksdptms <karthiksdofficial@gmail.com>
2025-02-25 11:28:40 +0530

    aaa

--- pm/src/App.jsx
+++ pm/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
        <Route path='/Studenttraining' element={<Studenttraining/>}/>
        <Route path='/Studentplacement' element={<Studentplacement/>}/>
        <h2>
-        ytuyaq
+        ytuyaqaaa
        </h2>
       </Routes>
       </BrowserRouter>
