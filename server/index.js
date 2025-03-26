13ec59275412c1d40f6eedcb1184d7602cb6b543
Gopikaprabakaran <Gopikapraba05@gmail.com>
2025-03-26 23:35:12 +0530

    local

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ connectDatabase();
 const app = express();
 app.use(cors(
   {
-    origin:[""],
+    origin:["https://pmsproject-pm.vercel.app/"],
     methods:["POST","GET"],
     credentials:true
   }
